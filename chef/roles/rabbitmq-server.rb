73a62b0df1b90e68294050e44a379acff545a26a
Keith Rochford <keith.rochford@gmail.com>
2013-07-31 15:07:14 +0100

    fixed recipe name rabbit

--- chef/roles/rabbitmq-server.rb
+++ chef/roles/rabbitmq-server.rb
@@ -2,6 +2,6 @@ name "rabbitmq-server"
 description "RabbiMQ server role - Setups the rabbitmq app"
 
 run_list(
-         "recipe[rabbitmq::rabbit]",
+         "recipe[rabbitmq]",
          "recipe[rabbitmq::monitor]"
 )
